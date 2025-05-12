f6c451f8318967693a44399b15f9a44ea6677adb
Hazal Yıldırım <hazal.yildirim12@outlook.com>
2025-05-12 18:52:01 +0300

    ...

---
+++ Video5-C#/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video5_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region alt alta 10 tane yıldız oluşturma
+
+            //for (int i = 1;i<=10;i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            //#endregion
+
+            //#region yan yana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            //#endregion
+            //#region satır sutun 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            //#endregion
+
+            //#region dik üçgen
+
+            //for (int i = 1; i <10 ; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //#endregion
+
+            //#region ters dik üçgen
+
+            //for (int i = 10; i >=1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //#endregion
+
+            //#region dik ve ters dik üçgen
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //    for (int i = 9; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //#endregion
+
+
+            //          #region piramit
+            //          int n = 5;
+
+            //          for (int i = 1; i <= n; i++)
+            //          {
+            //              for (int j = n - i; j > 0; j--)
+            //              {
+            //                  Console.Write(" ");
+            //              }
+
+
+            //          for (int k = 1; k <= 2 * i - 1; k++)
+            //          {
+            //              Console.Write("*");
+            //          }
+            //          Console.WriteLine();
+            //}
+            //          #endregion
+
+            //#region ters piramit
+            //int n = 5;
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //#endregion
+            Console.ReadKey();
+
+        }
+    }
+}
